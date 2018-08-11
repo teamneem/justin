390800efb8e4bba1e92041ce2e136f8005ed0c81
Neem Serra <nserr@monsanto.com>
2018-08-11 16:01:15 -0500

    Initial Commit

---
+++ Justin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Justin
+//
+//  Created by SERRA, NEEM [AG-Contractor/1000] on 8/11/18.
+//  Copyright © 2018 SERRA, NEEM [AG-Contractor/1000]. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
